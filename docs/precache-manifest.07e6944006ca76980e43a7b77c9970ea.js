91c1fbc7ef76805558211c6df0e9b968e155661b
barbara-chaves <barbarabarcellos90@gmail.com>
2019-10-17 23:11:12 +0200

    docs

---
+++ docs/precache-manifest.07e6944006ca76980e43a7b77c9970ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8670d49b7c706dd6115dfdf7dbff82c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "59083262c1da6937d655",
+    "url": "./static/css/main.4d3ba777.chunk.css"
+  },
+  {
+    "revision": "b9908899b74ca7c5392b",
+    "url": "./static/js/2.aaeead43.chunk.js"
+  },
+  {
+    "revision": "59083262c1da6937d655",
+    "url": "./static/js/main.c9f9999d.chunk.js"
+  },
+  {
+    "revision": "f2edb5ba7e19e24da3f7",
+    "url": "./static/js/runtime-main.e6cdb6f2.js"
+  },
+  {
+    "revision": "49255cb281084c9c58a3e8c86e743a16",
+    "url": "./static/media/Pokémon_logo.49255cb2.png"
+  }
+]);
